1722294277cf985ae34cba4bd751f78756b5db3f
Andrew Balson <andrewbalson@outlook.com>
2023-02-21 10:50:48 -0600

    Header

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -72,7 +72,7 @@ export class Navigation extends Component {
           </Link>
           <div className="Nav--Links">
             <NavLink to="/">Home</NavLink>
-            <NavLink to="/components/">Components</NavLink>
+            <NavLink to="/components/">Description</NavLink>
             <div
               className={`Nav--Group ${
                 this.state.activeSubNav === 'posts' ? 'active' : ''
@@ -95,7 +95,7 @@ export class Navigation extends Component {
                 Blog
                 <div className="Nav--GroupLinks">
                   <NavLink to="/blog/" className="Nav--GroupLink">
-                    All Posts
+                    Reviews
                   </NavLink>
                   {subNav.posts.map((link, index) => (
                     <NavLink
@@ -109,7 +109,7 @@ export class Navigation extends Component {
                 </div>
               </span>
             </div>
-            <NavLink to="/default/">Default</NavLink>
+            <NavLink to="/default/">About</NavLink>
             <NavLink to="/contact/">Contact</NavLink>
           </div>
           <button
