4afbf6bb7fb41611b38493b6703c01b26135445f
Andrew Balson <andrewbalson@outlook.com>
2023-02-22 22:54:26 -0600

    facebook

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,14 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      <a href="https://www.facebook.com/people/Mau-adventures-Atv-CR/100063765570457/">@Mau-Adventures</a>
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          <a href="https://thriveweb.com.au/">Andrew Balson</a>.
         </span>
       </div>
     </footer>
